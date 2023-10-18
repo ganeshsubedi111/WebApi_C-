fe78dc95b7881fc56fba5a95dfd9654d9b9abec9
ganeshsubedi111 <subediganesh306@gmail.com>
2023-10-18 21:30:32 +0545

    apicall in console

---
+++ TestWebApi/TestWebApi/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace TestWebApi
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            HttpClient client = new HttpClient();
+            HttpResponseMessage response = await client.GetAsync("https://fakestoreapi.com/products");
+
+            if (response.IsSuccessStatusCode)
+            {
+                string message = await response.Content.ReadAsStringAsync();
+                Console.WriteLine("Message from Web API: " + message);
+            }
+            else
+            {
+                Console.WriteLine("Error: " + response.StatusCode);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
